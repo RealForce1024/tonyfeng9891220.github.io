a30199d6abdbdfc1aeb776155708033060cd85dd
fqc <337940626@qq.com>
2017-03-27 14:11:12 +0800

    线程安全

---
+++ Test.java
@@ -0,0 +1,17 @@
+/**
+ * Test
+ */
+public class Test {
+    {
+        System.out.println("hello world");
+    }
+    public static void main(String[] args) {
+        //new Test();
+        int a = 23;
+        //byte b = 128;
+        byte b = 127;
+        //b = a; //will be compile wrong,because it should be convert by force
+         
+        System.out.println("hello main");
+    }
+}
